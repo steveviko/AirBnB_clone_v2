6c8edb25388ca1734bebf6fd5fa2badcc5395be6
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 <stevenviko@gmail.com>
2023-05-08 02:58:25 -0700

    Task 0. Prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Setup  web server for the deployment of Web_static.
+apt update -y
+apt install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <h5>Nginx server Test</h5>
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo service nginx restart
